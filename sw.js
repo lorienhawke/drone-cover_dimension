f4a26839cc8ce04544d72d735a908ec5590c6871
lorienhawke <crystalmind19@gmail.com>
2026-02-24 11:10:54 -0400

    initial upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'drone-calc-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
